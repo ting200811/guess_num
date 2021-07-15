46230255878bc50f47769b9572fe056f0b6ad1bd
Dabau <ting200811@hotmail.com>
2021-07-15 17:15:06 +0800

    final vers

--- guess.py
+++ guess.py
@@ -1,6 +1,11 @@
 # 猜數字練習
 import random
-r = random.randint(1, 100)
+strat = input('請輸入範圍起始值: ')
+end = input('請輸入範圍結束值: ')
+strat = int(strat)
+end = int(end)
+
+r = random.randint(strat, end)
 count = 0
 while True:
     num = input('請猜猜看數字:')

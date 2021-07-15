559f41a86a30626583157fc6d38e16c9b050b8b8
Dabau <ting200811@hotmail.com>
2021-07-15 17:09:59 +0800

    second vers

--- guess.py
+++ guess.py
@@ -1,14 +1,17 @@
 # 猜數字練習
 import random
 r = random.randint(1, 100)
-
+count = 0
 while True:
-    num = input('請猜猜看數字')
+    num = input('請猜猜看數字:')
     num = int(num)
+    count += 1
     if num == r:
         print('棒毆')
+        print('第', count,'次就猜到了')
         break
     elif num > r:
         print('比答案大')
     elif num < r:
         print('比答案小')
+    print('您猜了', count, '次')
